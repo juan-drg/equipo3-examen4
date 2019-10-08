5770e1e11e2716aba57a46d035042956b3059f59
Juan ACC <ing.juan.acc@gmail.com>
2019-10-08 11:33:27 -0500

    Se agrega servicioservice

--- equipo3-QuotationRestService/src/main/java/com/everis/quotationrestservice/service/ServicioService.java
+++ equipo3-QuotationRestService/src/main/java/com/everis/quotationrestservice/service/ServicioService.java
@@ -1,10 +1,19 @@
 package com.everis.quotationrestservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.everis.quotationrestservice.model.Servicio;
+import com.everis.quotationrestservice.repository.ServicioRepository;
+
 @Service
-public class ServicioRepository {
+public class ServicioService {
 	
+	@Autowired
+	private ServicioRepository servicioRepository;
 	
+	public Servicio buscarId(int idservicio) {
+		return servicioRepository.findById(idservicio).get();
+	}	
 	
 }
