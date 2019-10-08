efbf275539a7b5b837d18df265a999d1e47f58d8
volkodlak300 <volkodlak300@gmail.com>
2019-10-08 10:32:20 -0500

    computRestService
    nuevo proyecto

---
+++ equipo3-computerestservice/src/main/java/com/equipo3/computerestservice/Equipo3ComputerestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.equipo3.computerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo3ComputerestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo3ComputerestserviceApplication.class, args);
+	}
+
+}
---
+++ equipo3-computerestservice/src/test/java/com/equipo3/computerestservice/Equipo3ComputerestserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.equipo3.computerestservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Equipo3ComputerestserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
