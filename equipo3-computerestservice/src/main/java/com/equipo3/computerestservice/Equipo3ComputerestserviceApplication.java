5b5b4afc2baa2a8cc488e91eb8c1b6106ee4ebe5
YASMIN <rivera_15yas@outlook.com>
2019-10-08 13:06:24 -0500

    aplicacion_feign

--- equipo3-computerestservice/src/main/java/com/equipo3/computerestservice/Equipo3ComputerestserviceApplication.java
+++ equipo3-computerestservice/src/main/java/com/equipo3/computerestservice/Equipo3ComputerestserviceApplication.java
@@ -3,10 +3,18 @@ package com.equipo3.computerestservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class Equipo3ComputerestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo3ComputerestserviceApplication.class, args);
+	}
 
 
 }
